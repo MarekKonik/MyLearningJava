beedc3307a3abc2dbe53e5cba05331667ac5ccd6
Marek Konik <marekkonik.java@gmail.com>
2021-02-08 19:57:10 +0000

    Learn Java FX from course, and start new poroject cost car

---
+++ StrefaKursów/KursJavaFxSpringBootPraktycznyProjekt/src/main/java/sample/Main.java
@@ -0,0 +1,28 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    private static final String TILTE = "Marek Konik - tytuł";
+    private static int WIDTH = 600;
+    private static int HEIGHT = 400;
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+        primaryStage.setTitle(TILTE);
+        primaryStage.setScene(new Scene(root,WIDTH,HEIGHT));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
